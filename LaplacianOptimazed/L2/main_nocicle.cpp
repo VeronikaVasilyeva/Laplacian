ee9d9dbe9415fbd57549f0fecd2129dc601f7039
VV <VV@vv>
2017-05-30 18:22:11 +0500

    worked programm

--- LaplacianOptimazed/L2/main_nocicle.cpp
+++ LaplacianOptimazed/L2/main_nocicle.cpp
@@ -37,15 +37,47 @@ char* make_file_name(int i, char*& ext)
 	return filename;
 }
 
+void laplacian(int width, int height, unsigned char*& resourse, unsigned char*& result)
+{
+	for (int y = 1; y < height - 1; y++)
+	{
+		for (int x = 1; x < width - 1; x++)
+		{
+			//applay_filter(x, y, chnl);
+			double color0 = 0;
+			double color1 = 0;
+			double color2 = 0;
+
+			for (int i = -1; i <= 1; i++)
+			{
+				for (int j = -1; j <= 1; j++)
+				{
+					int tmp = 3 * (width *(y + j) + x + i);
+					int filter_i = filter[i + 1][j + 1];
+					color0 += filter_i * resourse[tmp];		//get_pixel(x + i, y + j, chnl);
+					color1 += filter_i * resourse[tmp + 1]; //get_pixel(x + i, y + j, chnl);
+					color2 += filter_i * resourse[tmp + 2]; //get_pixel(x + i, y + j, chnl);
+				}
+			}
+
+			//set_pixel(x, y, chnl, color);
+			int tmp = 3 * (width * y + x);
+			result[tmp] = static_cast<unsigned char>(max(0.0, min(color0, 255.0)));		//(std::max(0.0, std::min(clr, 255.0)));
+			result[tmp + 1] = static_cast<unsigned char>(max(0.0, min(color1, 255.0))); //(std::max(0.0, std::min(clr, 255.0)));
+			result[tmp + 2] = static_cast<unsigned char>(max(0.0, min(color2, 255.0))); //(std::max(0.0, std::min(clr, 255.0)));
+		}
+	}
+}
+
 int main(void)
 {
 	double start_time = clock();
 
 	int width, height, bpp;
 	char* file_sourse;
-	char* file_result;
 	unsigned char* resourse;
-	unsigned char* result;
+	unsigned char* result ;
+	char* file_result;
 	int img;
 
 	int num_thread = omp_get_num_procs();
@@ -53,41 +85,15 @@ int main(void)
 	for (img = 1; img < 123; img++)
 	{
 		//load img
+		width, height, bpp;
 		file_sourse = make_file_name(img, extension);
 		resourse = stbi_load(file_sourse, &width, &height, &bpp, 3);
-
+		
 		//init result img
 		result = static_cast<unsigned char*>(calloc(width * height * 3, sizeof(unsigned char)));
 
 		//laplacian();
-		for (int y = 1; y < height - 1; y++)
-		{
-			for (int x = 1; x < width - 1; x++)
-			{
-				//applay_filter(x, y, chnl);
-				double color0 = 0;
-				double color1 = 0;
-				double color2 = 0;
-
-				for (int i = -1; i <= 1; i++)
-				{
-					for (int j = -1; j <= 1; j++)
-					{
-						int tmp = 3 * (width *(y + j) + x + i);
-						int filter_i = filter[i + 1][j + 1];
-						color0 += filter_i * resourse[tmp];		//get_pixel(x + i, y + j, chnl);
-						color1 += filter_i * resourse[tmp + 1]; //get_pixel(x + i, y + j, chnl);
-						color2 += filter_i * resourse[tmp + 2]; //get_pixel(x + i, y + j, chnl);
-					}
-				}
-
-				//set_pixel(x, y, chnl, color);
-				int tmp = 3 * (width * y + x);
-				result[tmp] = static_cast<unsigned char>(max(0.0, min(color0, 255.0)));		//(std::max(0.0, std::min(clr, 255.0)));
-				result[tmp + 1] = static_cast<unsigned char>(max(0.0, min(color1, 255.0))); //(std::max(0.0, std::min(clr, 255.0)));
-				result[tmp + 2] = static_cast<unsigned char>(max(0.0, min(color2, 255.0))); //(std::max(0.0, std::min(clr, 255.0)));
-			}
-		}
+		laplacian(width, height, resourse, result);
 
 		//write to file
 		file_result = make_file_name(img, extension1);
@@ -103,6 +109,6 @@ int main(void)
 
 	printf("runtime = \"%lf\" \n", static_cast<double>(search_time)); // גנולÿ נאבמעû ןנמדנאללû      
 	system("pause");
-
+	
 	return 0;
 }
